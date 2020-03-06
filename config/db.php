a3baf4ccbd5658250b6320fea58ea146c223763c
Andrew Pogulailo <pogulailo.a@gmail.com>
2020-03-06 09:03:42 +0200

    Configured the Yii 2 framework.

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=john_website',
+    'username' => 'john_website',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
